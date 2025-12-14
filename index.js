9c17562e2279fb026878b804c959cac74c3a4236
Muhammad Zeeshan Tayyab <zitayyab17@gmail.com>
2025-12-14 23:35:53 +0000

    initial commit - v2

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Render Auto Deploy V2!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
